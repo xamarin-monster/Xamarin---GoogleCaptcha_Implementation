2d9e2f75b12bad8d5282691695f51e6c35dbc562
Amarjeet Singh <amar.bst1998@gmail.com>
2024-02-03 15:29:54 +0530

    Code for Google Captcha Implementation.

---
+++ GoogleCaptcha/GoogleCaptcha/GoogleCaptcha/RecaptchaService/IRecaptchaService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GoogleCaptcha.RecaptchaService
+{
+    public interface IReCaptchaService
+    {
+        Task<string> Verify(string siteKey, string domainUrl);
+    }
+}
+
